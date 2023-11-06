14ac2683c0a4373b59255abb6a74024d6d4d78bc
Adam Baqaie <z5362016@ad.unsw.edu.au>
2023-11-06 18:02:59 +1100

    feature: Added enemies goal

--- src/main/java/dungeonmania/goals/GoalEnemies.java
+++ src/main/java/dungeonmania/goals/GoalEnemies.java
@@ -1,11 +1,25 @@
 package dungeonmania.goals;
 
+import java.util.List;
+
 import dungeonmania.Game;
+import dungeonmania.entities.enemies.ZombieToastSpawner;
 
 public class GoalEnemies implements GoalStrategy {
+    private int target;
+
+    public GoalEnemies(int target) {
+        this.target = target;
+    }
+
     @Override
     public boolean achieved(Game game) {
-        // TODO implement
+        List<ZombieToastSpawner> spawners = game.getMap().getEntities(ZombieToastSpawner.class);
+        if (spawners.isEmpty()) {
+            if (game.getEnemiesKilled() >= target) {
+                return true;
+            }
+        }
         return false;
     }
 
