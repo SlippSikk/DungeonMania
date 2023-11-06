fb3e10005b33d658ae7a83d68bd95510db850c80
Vinson Cai <z5420496@student.unsw.edu.au>
2023-11-06 13:31:28 +1100

    test: edited tests for microevolution

--- src/test/java/dungeonmania/mvp/task2/MicroevolutionTest.java
+++ src/test/java/dungeonmania/mvp/task2/MicroevolutionTest.java
@@ -4,6 +4,7 @@ import dungeonmania.DungeonManiaController;
 import dungeonmania.mvp.TestUtils;
 import dungeonmania.response.models.DungeonResponse;
 import dungeonmania.util.Direction;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -13,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MicroevolutionTest {
     @Test
     @Tag("16-0")
-    @DisplayName("Test achieving a basic enemy goal")
+    @DisplayName("Test dungeonmania controller can get enemies goal")
     public void testEnemyGoal() {
         DungeonManiaController dmc = new DungeonManiaController();
         DungeonResponse res = dmc.newGame("d_microevolutionTest_enemy", "c_microevolutionTest_enemy");
@@ -50,6 +51,10 @@ public class MicroevolutionTest {
     //     res = dmc.tick(Direction.DOWN);
     //     assertTrue(TestUtils.getGoals(res).contains(":enemies"));
 
+    //     // move player right to get sword
+    //     res = dmc.tick(Direction.RIGHT);
+    //     assertTrue(TestUtils.getGoals(res).contains(":enemies"));
+
     //     // move right destroy spawner
     //     res = dmc.tick(Direction.RIGHT);
     //     assertFalse(TestUtils.getGoals(res).contains(":enemies"));
@@ -63,23 +68,23 @@ public class MicroevolutionTest {
     //     String config = "c_microevolutionTest_multipleEnemies";
     //     DungeonResponse res = dmc.newGame("d_microevolutionTest_multipleEnemies", config);
 
-    //     // move player right and kill spider
-    //     res = dmc.tick(Direction.RIGHT);
-    //     assertTrue(TestUtils.getGoals(res).contains(":enemies"));
-
-    //     // move right destroy spawner
-    //     res = dmc.tick(Direction.RIGHT);
-    //     assertTrue(TestUtils.getGoals(res).contains(":enemies"));
-
-    //     // player (3, 2) spider (4, 3)
+    //     // player begins at (2, 0) and moves down to (2, 1) to collect sword
     //     res = dmc.tick(Direction.DOWN);
     //     assertTrue(TestUtils.getGoals(res).contains(":enemies"));
 
-    //     // player (3, 3) spider (4, 4)
+    //     // player moves left to (1, 1) and kills spider
+    //     res = dmc.tick(Direction.LEFT);
+    //     assertTrue(TestUtils.getGoals(res).contains(":enemies"));
+
+    //     // player moves to (1, 2) and destroys spawner
     //     res = dmc.tick(Direction.DOWN);
     //     assertTrue(TestUtils.getGoals(res).contains(":enemies"));
 
-    //     // player kills spider at (3, 4)
+    //     // player moves to (1, 3)
+    //     res = dmc.tick(Direction.DOWN);
+    //     assertTrue(TestUtils.getGoals(res).contains(":enemies"));
+
+    //     // player moves to (1, 4) and kills spider
     //     res = dmc.tick(Direction.DOWN);
     //     assertFalse(TestUtils.getGoals(res).contains(":enemies"));
     // }
@@ -92,60 +97,70 @@ public class MicroevolutionTest {
     //     String config = "c_microevolutionTest_complexGoalEnemies";
     //     DungeonResponse res = dmc.newGame("d_microevolutionTest_complexGoalEnemies", config);
 
-    //     // move player right and kill spider
-    //     res = dmc.tick(Direction.RIGHT);
     //     assertTrue(TestUtils.getGoals(res).contains(":enemies"));
     //     assertTrue(TestUtils.getGoals(res).contains(":exit"));
 
-    //     // move right destroy spawner
-    //     res = dmc.tick(Direction.RIGHT);
-    //     assertTrue(TestUtils.getGoals(res).contains(":enemies"));
-    //     assertTrue(TestUtils.getGoals(res).contains(":exit"));
-
-    //     // player (3, 2) spider (4, 3)
+    //     // player begins at (2, 0) and moves down to (2, 1) to collect sword
     //     res = dmc.tick(Direction.DOWN);
     //     assertTrue(TestUtils.getGoals(res).contains(":enemies"));
     //     assertTrue(TestUtils.getGoals(res).contains(":exit"));
 
-    //     // player (3, 3) spider (4, 4)
+    //     // player moves left to (1, 1) and kills spider
+    //     res = dmc.tick(Direction.LEFT);
+    //     assertTrue(TestUtils.getGoals(res).contains(":enemies"));
+    //     assertTrue(TestUtils.getGoals(res).contains(":exit"));
+
+    //     // player moves to (1, 2) and destroys spawner
     //     res = dmc.tick(Direction.DOWN);
     //     assertTrue(TestUtils.getGoals(res).contains(":enemies"));
     //     assertTrue(TestUtils.getGoals(res).contains(":exit"));
 
-    //     // player kills spider at (3, 4)
+    //     // player moves to (1, 3)
+    //     res = dmc.tick(Direction.DOWN);
+    //     assertTrue(TestUtils.getGoals(res).contains(":enemies"));
+    //     assertTrue(TestUtils.getGoals(res).contains(":exit"));
+
+    //     // player moves to (1, 4) and kills spider
+    //     res = dmc.tick(Direction.DOWN);
+    //     assertTrue(TestUtils.getGoals(res).contains(":enemies"));
+    //     assertTrue(TestUtils.getGoals(res).contains(":exit"));
+
+    //     // player moves to (1, 5) and destroys spawner
     //     res = dmc.tick(Direction.DOWN);
     //     assertFalse(TestUtils.getGoals(res).contains(":enemies"));
     //     assertTrue(TestUtils.getGoals(res).contains(":exit"));
 
+    //     // move player down and exit at (1, 6)
     //     res = dmc.tick(Direction.DOWN);
     //     assertFalse(TestUtils.getGoals(res).contains(":enemies"));
     //     assertFalse(TestUtils.getGoals(res).contains(":exit"));
     // }
 
-    // @Test
-    // @Tag("16-5")
-    // @DisplayName("Test complex goal: enemies AND boulders AND exit")
-    // public void complexGoalEnemies2() {
-    //     DungeonManiaController dmc = new DungeonManiaController();
-    //     String config = "c_microevolutionTest_complexGoalEnemies2";
-    //     DungeonResponse res = dmc.newGame("d_microevolutionTest_complexGoalEnemies2", config);
-    // }
+    // optional tests: may or may not implement later depending on time
+    @Test
+    @Tag("16-5")
+    @DisplayName("Test complex goal: enemies AND boulders AND exit")
+    public void complexGoalEnemies2() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        String config = "c_microevolutionTest_complexGoalEnemies2";
+        DungeonResponse res = dmc.newGame("d_microevolutionTest_complexGoalEnemies2", config);
+    }
 
-    // @Test
-    // @Tag("16-6")
-    // @DisplayName("Test complex goal: boulders AND treasure OR enemies")
-    // public void complexGoalEnemies3() {
-    //     DungeonManiaController dmc = new DungeonManiaController();
-    //     String config = "c_microevolutionTest_complexGoalEnemies3";
-    //     DungeonResponse res = dmc.newGame("d_microevolutionTest_complexGoalEnemies3", config);
-    // }
+    @Test
+    @Tag("16-6")
+    @DisplayName("Test complex goal: boulders AND treasure OR enemies")
+    public void complexGoalEnemies3() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        String config = "c_microevolutionTest_complexGoalEnemies3";
+        DungeonResponse res = dmc.newGame("d_microevolutionTest_complexGoalEnemies3", config);
+    }
 
-    // @Test
-    // @Tag("16-7")
-    // @DisplayName("Test complex goal enemies: boulders AND treasure OR enemies 2")
-    // public void complexGoalEnemies4() {
-    //     DungeonManiaController dmc = new DungeonManiaController();
-    //     String config = "c_microevolutionTest_complexGoalEnemies3";
-    //     DungeonResponse res = dmc.newGame("d_microevolutionTest_complexGoalEnemies3", config);
-    // }
+    @Test
+    @Tag("16-7")
+    @DisplayName("Test complex goal enemies: boulders AND treasure OR enemies 2")
+    public void complexGoalEnemies4() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        String config = "c_microevolutionTest_complexGoalEnemies3";
+        DungeonResponse res = dmc.newGame("d_microevolutionTest_complexGoalEnemies3", config);
+    }
 }
