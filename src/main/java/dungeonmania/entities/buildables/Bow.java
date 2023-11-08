f8d405e126b491cb8a40f9b9ec3be3aa3c93accc
Adam Baqaie <z5362016@ad.unsw.edu.au>
2023-11-08 21:40:17 +1100

    refactor: add back buildable refactoring

--- src/main/java/dungeonmania/entities/buildables/Bow.java
+++ src/main/java/dungeonmania/entities/buildables/Bow.java
@@ -1,22 +1,10 @@
 package dungeonmania.entities.buildables;
 
-import dungeonmania.Game;
 import dungeonmania.battles.BattleStatistics;
 
 public class Bow extends Buildable {
-    private int durability;
-
     public Bow(int durability) {
-        super(null);
-        this.durability = durability;
-    }
-
-    @Override
-    public void use(Game game) {
-        durability--;
-        if (durability <= 0) {
-            game.removeBuildable(this);
-        }
+        super(null, durability);
     }
 
     @Override
@@ -24,8 +12,4 @@ public class Bow extends Buildable {
         return BattleStatistics.applyBuff(origin, new BattleStatistics(0, 0, 0, 2, 1));
     }
 
-    @Override
-    public int getDurability() {
-        return durability;
-    }
 }
